3f8ea2147c2bc3c4f1eccdcc57b9262cdf48efd8
Kyle Cole <kylepcole@gmail.com>
2019-10-20 23:57:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,8 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(library,emoji)
-  load_library(library)[:get_emoticon][emoji]
+  output = load_library(library)[:get_emoticon][emoji]
+  output ? output : "Sorry, that emoji was not found"
 end
 
 def get_english_meaning(library, japanese_emoji)
